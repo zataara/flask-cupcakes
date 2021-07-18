ba8e79018d269cc165a54916a37a89208f880762
zataara <bryantmac@gmail.com>
2021-07-18 14:28:51 -0700

    setting up models/seed file connection

--- models.py
+++ models.py
@@ -7,18 +7,29 @@ def connect_db(app):
         db.init_app(app)
 
 
-class Pet(db.Model):
+DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+class Cupcake(db.Model):
     '''Database model for Users'''
 
-    __tablename__ = ''
+    __tablename__ = 'cupcakes'
 
     def __repr__(self):
         
         u = self
-        return f'<User {u.id} '
+        return f'<Cupcake {u.id} '
 
     id = db.Column(db.Integer,
                     primary_key=True,
                     autoincrement=True)
+    flavor = db.Column(db.Text,
+                        nullable=False)
+    size = db.Column(db.Text,
+                        nullable=False)
+    rating = db.Column(db.Integer,
+                        nullable=False)
+    image = db.Column(db.Text,
+                        nullable=False,
+                        default=DEFUALT_IMAGE)
     
 
