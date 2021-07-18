9f6a38eb4dcead1dee98f6f5540a1d124ae61b97
zataara <bryantmac@gmail.com>
2021-07-18 13:26:58 -0700

    added basic seed file data

--- seed.py
+++ seed.py
@@ -5,5 +5,19 @@ from app import app
 db.drop_all()
 db.create_all()
 
+c1 = Cupcake(
+    flavor="cherry",
+    size="large",
+    rating=5,
+)
 
+c2 = Cupcake(
+    flavor="chocolate",
+    size="small",
+    rating=9,
+    image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
+)
+
+db.session.add_all([c1, c2])
+db.session.commit()
 
