b8c476f6a453a533921978b21082d752e6a52e65
zataara <bryantmac@gmail.com>
2021-07-20 11:42:00 -0700

    added event listener to handle form for adding new cupcake

--- static/js/app.js
+++ static/js/app.js
@@ -16,4 +16,36 @@ function generateCupcakeHTML(cupcake) {
         `;
 }
 
+//Render cupcakes onto page from API
+
+
+async function showInitialCupcakes() {
+    const response = await axios.get(`${BASE_URL}/cupcakes`);
+
+    for (let cupcakeData of response.data.cupcakes) {
+        let newCupcake = $(generateCupcakeHTML(cupcakeData));
+        $('#cupcakes-list').append(newCupcake);
+    }
+}
+
+
+//handle form for adding new cupcake
+
+$('#new-cupcake-form').on('submit', async function(evt) {
+    evt.preventDefault();
+
+    let flavor = $('flavor').val();
+    let rating = $('rating').val();
+    let size = $('size').val();
+    let image = $('image').val();
+
+    const newCupcakeResponse = await axios.post(`${BASE_URL}/cupcakes`, {
+        flavor, rating, size, image
+    });
+
+    let newCupcake = $(generateCupcakeHTML(newCupcakeResponse.data.cupcake));
+    $('#cupcakes-list').append(newCupcake);
+    $('#new-cupcake-form').trigger('reset');
+});
+
 
