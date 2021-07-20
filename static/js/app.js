0f74ba96fd9a6252b7070b8a3dc6eaef3f6e30e6
zataara <bryantmac@gmail.com>
2021-07-20 12:29:34 -0700

    fixed syntax issues

--- static/js/app.js
+++ static/js/app.js
@@ -35,12 +35,15 @@ $('#new-cupcake-form').on('submit', async function(evt) {
     evt.preventDefault();
 
     let flavor = $('flavor').val();
-    let rating = $('rating').val();
     let size = $('size').val();
+    let rating = $('rating').val();
     let image = $('image').val();
 
     const newCupcakeResponse = await axios.post(`${BASE_URL}/cupcakes`, {
-        flavor, rating, size, image
+        flavor, 
+        rating, 
+        size, 
+        image
     });
 
     let newCupcake = $(generateCupcakeHTML(newCupcakeResponse.data.cupcake));
@@ -48,4 +51,16 @@ $('#new-cupcake-form').on('submit', async function(evt) {
     $('#new-cupcake-form').trigger('reset');
 });
 
+//handle deleting a cupcake
+
+$('#cupcakes-list').on('click', '.delete-button', async function(evt) {
+    evt.preventDefault();
+    let $cupcake = $(evt.target).closest('div');
+    let cupcakeId = $cupcake.attr('data-cupcake-id');
+
+    await axios.delete(`${BASE_URL}/cupcakes/${cupcakeId}`);
+    $cupcake.remove();
+});
+
+$(showInitialCupcakes);
 
